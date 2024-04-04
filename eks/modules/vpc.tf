49e2fe5231e0d3c778289fdc61a065863de0b42a
aesperer <piyrw9754@gmail.com>
2024-04-04 14:46:09 +0900

    eks provider vpc module

---
+++ eks/modules/vpc.tf
@@ -0,0 +1,15 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "stw-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = data.aws_availability_zones.available.names
+  private_subnets = ["10.0.0.0/22", "10.0.4.0/22", "10.0.8.0/22"]
+  public_subnets  = ["10.0.100.0/22", "10.0.104.0/22", "10.0.108.0/22"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+  enable_vpn_gateway = false
+
+}
