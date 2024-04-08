f17415dfc3f919c778e1cb7fdbed2b0a2f029239
aesperer <piyrw9754@gmail.com>
2024-04-08 16:45:14 +0900

    add subnet

--- tutorial/vpc.tf
+++ tutorial/vpc.tf
@@ -4,11 +4,8 @@ resource "aws_vpc" "main" {
   instance_tenancy = "default"
 
   enable_dns_support = true
-
   enable_dns_hostnames = true
-
   enable_classiclink = false
-
   enable_classiclink_dns_support = false
 
   assign_generated_ipv6_cidr_block = false
@@ -16,6 +13,7 @@ resource "aws_vpc" "main" {
   tags = {
     Name = "main"
   }
+  
 }
 
 output "vpc_id" {
