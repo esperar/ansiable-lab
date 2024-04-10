8eba6da8c57b9a77b395e83e4e386abdbe60db2e
aesperer <piyrw9754@gmail.com>
2024-04-10 22:42:14 +0900

    add k8s app yaml

--- tutorial/routing_tables.tf
+++ tutorial/routing_tables.tf
@@ -1,43 +1,43 @@
 resource "aws_route_table" "public" {
-    vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.main.id
 
-    route {
-        cidr_block = "0.0.0.0/0"
+  route {
+    cidr_block = "0.0.0.0/0"
 
-        gateway_id = aws_internet_gateway.main.id
-    }
+    gateway_id = aws_internet_gateway.main.id
+  }
 
-    tags = {
-        Name = "public"
-    }
+  tags = {
+    Name = "public"
+  }
 }
 
 resource "aws_route_table" "private1" {
-    vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.main.id
 
-    route {
-        cidr_block = "0.0.0.0/0"
+  route {
+    cidr_block = "0.0.0.0/0"
 
-        gateway_id = aws_nat_gateway.gw1.id
-    }
+    gateway_id = aws_nat_gateway.gw1.id
+  }
 
-    tags = {
-        Name = "private1"
-    }
+  tags = {
+    Name = "private1"
+  }
 }
 
 resource "aws_route_table" "private2" {
-    vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.main.id
 
-    route {
-        cidr_block = "0.0.0.0/0"
+  route {
+    cidr_block = "0.0.0.0/0"
 
-        gateway_id = aws_nat_gateway.gw2.id
-    }
+    gateway_id = aws_nat_gateway.gw2.id
+  }
 
-    tags = {
-        Name = "private2"
-    }
+  tags = {
+    Name = "private2"
+  }
 }
 
 
